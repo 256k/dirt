fbad2e8fb37fe3e23a7078de18fa8e2cff367777
fayek helmi <fayek.helmi@gmail.com>
2021-11-25 15:37:36 +0000

    initial setup of rpi dirt server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const {readFile} = require('fs').promises;
+const app = express();
+
+app.get('/', async (request,response) => {
+response.send(await readFile('./index.html', 'utf8'));
+});
+
+app.get('/wow', async (request,response) => {
+response.send(await readFile('./wow.html', 'utf8'));
+});
+
+app.listen(process.env.PORT || 3000, () => {console.log("You're in, kid.")});
